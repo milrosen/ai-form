7b557cc4703f7dbd2ade73eb0c1cf804e817ff8e
milrosen <61394098+milrosen@users.noreply.github.com>
2023-10-25 16:45:26 -0400

    runtime edge

--- svelte.config.js
+++ svelte.config.js
@@ -4,19 +4,18 @@ import {
 } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+import adapter from '@sveltejs/adapter-vercel';
 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			runtime: 'edge'
-		})
+		}),
+
+		inlineStyleThreshold: 5000
 	}
 };
+
 
 export default config;
