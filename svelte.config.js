5091616469cb2885b9a34e2b6cb54997713861f4
milrosen <61394098+milrosen@users.noreply.github.com>
2023-10-25 16:46:45 -0400

    runtime edge take 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,4 @@
 import adapter from '@sveltejs/adapter-vercel';
-import {
-	vitePreprocess
-} from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,6 +10,5 @@ const config = {
 		inlineStyleThreshold: 5000
 	}
 };
-
 
 export default config;
